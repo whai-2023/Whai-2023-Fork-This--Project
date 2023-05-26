0cf8dd3d7e0ddcd29cb67ea19d9aadc431ec178a
Michael Jansen <michael.stradiv@gmail.com>
2023-05-26 15:03:55 +1200

    update users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -20,10 +20,16 @@ router.get('/customize', async (req, res) => {
   try {
     const lavaColors = await db.getLavaColors()
     const baseColors = await db.getBaseColors()
-    const bodyPrice = (lavaColors[0].price)
-    const basePrice = (baseColors[0].price) 
+    const bodyPrice = parseInt(lavaColors.price)
+    const basePrice = parseInt(baseColors.price)
     const totalPrice = db.calculateTotalPrice(bodyPrice, basePrice)
-    res.render('customize', { body: lavaColors, base: baseColors, totalPrice: totalPrice.total_price })
+    res.render('customize', {
+      body: lavaColors,
+      base: baseColors,
+      totalPrice: totalPrice,
+      selectedLavaImage: `/images/lavaLampBody/${lavaColors[0].image_path}.png`,
+      selectedBaseImage: `/images/lavaLampBase/${baseColors[0].image_path}.png`,
+    });
   } catch (err) {
     res.send('DATABASE ERROR: ' + 'Customize Route Problem')
   }
